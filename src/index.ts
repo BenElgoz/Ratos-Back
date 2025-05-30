1fbdf791af24add7c8d26bfcfbeb142f9c09cbd3
Benjamin Bonnevial <benjamin.bonnevial@gmail.com>
2025-05-30 15:15:41 +0200

    test cors

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ import authRoutes from './auth/auth.routes';
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*',
+}));app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 // app.use('/api/protected', protectedRoutes);
