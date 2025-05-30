0976044f5a722b3a6440f41f93a2b44d9f3926d9
Benjamin Bonnevial <benjamin.bonnevial@gmail.com>
2025-05-30 18:40:31 +0200

    cors methods

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './auth/auth.routes';
-// import protectedRoutes from './protected/protected.routes';
 
 dotenv.config();
 
 const app = express();
 app.use(cors({
   origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }));app.use(express.json());
 
 app.use('/auth', authRoutes);
-// app.use('/api/protected', protectedRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('RATOS BACK 🧀 API is running 🚀');
