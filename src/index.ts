1b6a84efa9ba184bb1d1b2894038036ebaf5f3d6
BenB <benjamin.bonnevial@gmail.com>
2025-06-04 11:00:48 +0200

    rm /

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,12 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000', 
-  'https://ratos.fr/',
-  'https://ratos.fr/',
-  'http://www.ratos.fr/',
-  'https://www.ratos.fr/',
-  'http://landing.ratos.fr/',
-  'https://landing.ratos.fr/'
+  'https://ratos.fr',
+  'https://ratos.fr',
+  'http://www.ratos.fr',
+  'https://www.ratos.fr',
+  'http://landing.ratos.fr',
+  'https://landing.ratos.fr'
 ];
 
 app.use(cors({
